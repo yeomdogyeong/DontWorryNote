267054383c319efe84b0a59c5b42f7c5cbfe6f2f
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-08 23:02:10 +0900

    Feat:survey usesearchParams 경로 구현

--- src/components/Dummy/index.tsx
+++ src/components/Dummy/index.tsx
@@ -6,28 +6,150 @@ import { School } from "../Icon/School";
 import { Sleep } from "../Icon/Sleep";
 
 export const listItem = [
+  {},
   {
-    text: "업무 또는 학업",
-    srcImg: <School />,
+    title: {
+      one: "하루에 가장 많은 시간을 쓰는",
+      two: "쓰는 활동은 무엇인가요?",
+    },
+    items: [
+      { text: "업무 또는 학업", srcImg: <School /> },
+      {
+        text: "생산적인 자기계발",
+        srcImg: <Burning />,
+      },
+      {
+        text: "휴식과 수면",
+        srcImg: <Sleep />,
+      },
+      {
+        text: "취미와 여가활동",
+        srcImg: <Drawing />,
+      },
+      {
+        text: "운동 등 셀프 케어",
+        srcImg: <Muscle />,
+      },
+      {
+        text: "가족과 친구와의 시간",
+        srcImg: <Family />,
+      },
+    ],
   },
   {
-    text: "생산적인 자기계발",
-    srcImg: <Burning />,
+    title: {
+      one: "가장 가치있다고",
+      two: "느끼는것은 무엇인가요?",
+    },
+    items: [
+      { text: "끊임없는 자기 발전", srcImg: <School /> },
+      {
+        text: "행복과 만족",
+        srcImg: <Burning />,
+      },
+      {
+        text: "성취감과 목표달성",
+        srcImg: <Sleep />,
+      },
+      {
+        text: "여유와 휴식",
+        srcImg: <Drawing />,
+      },
+      {
+        text: "효율성과 생산성",
+        srcImg: <Muscle />,
+      },
+      {
+        text: "가족과 친구와의 유대감",
+        srcImg: <Family />,
+      },
+    ],
   },
   {
-    text: "휴식과 수면",
-    srcImg: <Sleep />,
+    title: {
+      one: "이상적인 휴가계획은",
+      two: "무엇이라고 생각하나요?",
+    },
+    items: [
+      { text: "자기계발 및 독서", srcImg: <School /> },
+      {
+        text: "스파나 마사지로 힐링",
+        srcImg: <Burning />,
+      },
+      {
+        text: "워크숍 및 세미나 참석",
+        srcImg: <Sleep />,
+      },
+      {
+        text: "여유롭고 느긋한 휴식",
+        srcImg: <Drawing />,
+      },
+      {
+        text: "강의 수강 및 자격증 취득",
+        srcImg: <Muscle />,
+      },
+      {
+        text: "집에서 영화나 드라마 몰아보기",
+        srcImg: <Family />,
+      },
+    ],
   },
   {
-    text: "취미와 여가활동",
-    srcImg: <Drawing />,
+    title: {
+      one: "스트레스를 받을 때",
+      two: "주로 어떻게 해소하나요?",
+    },
+    items: [
+      { text: "운동으로 스트레스 해소", srcImg: <School /> },
+      {
+        text: "가족이나 친구와의 대화",
+        srcImg: <Burning />,
+      },
+      {
+        text: "음악 감상 또는 명상",
+        srcImg: <Sleep />,
+      },
+      {
+        text: "계획을 세우고 문제를 해결",
+        srcImg: <Drawing />,
+      },
+      {
+        text: "취미 활동 즐기기",
+        srcImg: <Muscle />,
+      },
+      {
+        text: "새로운 프로젝트나 도전에 집중",
+        srcImg: <Family />,
+      },
+    ],
   },
   {
-    text: "운동 등 셀프 케어",
-    srcImg: <Muscle />,
-  },
-  {
-    text: "가족과 친구와의 시간",
-    srcImg: <Family />,
+    title: {
+      one: "내가 원하는 나의 모습은",
+      two: "어떤 모습인가요?",
+    },
+    items: [
+      { text: "목표지향적 개짱이", srcImg: <School /> },
+      {
+        text: "똑똑한 개짱이",
+        srcImg: <Burning />,
+      },
+      {
+        text: "여유로운 개짱이",
+        srcImg: <Sleep />,
+      },
+      {
+        text: "계획적인 개짱이",
+        srcImg: <Drawing />,
+      },
+      {
+        text: "행복한 개짱이",
+        srcImg: <Muscle />,
+      },
+      {
+        text: "즐거운 개짱이",
+        srcImg: <Family />,
+      },
+    ],
   },
 ];
