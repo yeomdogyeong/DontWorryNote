fd81f10724cbea9b1cd69f544f76df494f5c9cec
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-11 22:55:41 +0900

    Fix:TypeBug fix후 SurveyResult적용

--- src/components/Dummy/index.tsx
+++ src/components/Dummy/index.tsx
@@ -4,7 +4,13 @@ import { Family } from "../Icon/Family";
 import { Muscle } from "../Icon/Muscle";
 import { School } from "../Icon/School";
 import { Sleep } from "../Icon/Sleep";
-
+import baejjange1 from "../../../public/baejjange3.png";
+import baejjange2 from "../../../public/baejjange1.png";
+import minibaejjange from "../../../public/Ellipse1.png";
+import minigaemi from "../../../public/Ellipse2.png";
+import gaemi from "../../../public/gaemi1.png";
+import gaemi2 from "../../../public/gaemi2.png";
+import { SurveyList } from "@/types/common";
 export const listItem = [
   {},
   {
@@ -13,31 +19,31 @@ export const listItem = [
       two: "쓰는 활동은 무엇인가요?",
     },
     items: [
-      { text: "업무 또는 학업", srcImg: <School />, score: 3 },
+      { text: "업무 또는 학업", srcImg: <School />, score: 1 },
       {
         text: "생산적인 자기계발",
         srcImg: <Burning />,
-        score: 3,
+        score: 1,
       },
       {
         text: "휴식과 수면",
         srcImg: <Sleep />,
-        score: 3,
+        score: 0,
       },
       {
         text: "취미와 여가활동",
         srcImg: <Drawing />,
-        score: 3,
+        score: 0,
       },
       {
         text: "운동 등 셀프 케어",
         srcImg: <Muscle />,
-        score: 3,
+        score: 1,
       },
       {
         text: "가족과 친구와의 시간",
         srcImg: <Family />,
-        score: 3,
+        score: 0,
       },
     ],
   },
@@ -47,31 +53,31 @@ export const listItem = [
       two: "느끼는것은 무엇인가요?",
     },
     items: [
-      { text: "끊임없는 자기 발전", srcImg: <School />, score: 3 },
+      { text: "끊임없는 자기 발전", srcImg: <School />, score: 1 },
       {
         text: "행복과 만족",
         srcImg: <Burning />,
-        score: 3,
+        score: 0,
       },
       {
         text: "성취감과 목표달성",
         srcImg: <Sleep />,
-        score: 3,
+        score: 1,
       },
       {
         text: "여유와 휴식",
         srcImg: <Drawing />,
-        score: 3,
+        score: 0,
       },
       {
         text: "효율성과 생산성",
         srcImg: <Muscle />,
-        score: 3,
+        score: 1,
       },
       {
         text: "가족과 친구와의 유대감",
         srcImg: <Family />,
-        score: 3,
+        score: 0,
       },
     ],
   },
@@ -81,31 +87,31 @@ export const listItem = [
       two: "무엇이라고 생각하나요?",
     },
     items: [
-      { text: "자기계발 및 독서", srcImg: <School /> },
+      { text: "자기계발 및 독서", srcImg: <School />, score: 1 },
       {
         text: "스파나 마사지로 힐링",
         srcImg: <Burning />,
-        score: 3,
+        score: 0,
       },
       {
         text: "워크숍 및 세미나 참석",
         srcImg: <Sleep />,
-        score: 3,
+        score: 1,
       },
       {
         text: "여유롭고 느긋한 휴식",
         srcImg: <Drawing />,
-        score: 3,
+        score: 0,
       },
       {
         text: "강의 수강 및 자격증 취득",
         srcImg: <Muscle />,
-        score: 3,
+        score: 1,
       },
       {
         text: "집에서 영화나 드라마 몰아보기",
         srcImg: <Family />,
-        score: 3,
+        score: 0,
       },
     ],
   },
@@ -115,31 +121,31 @@ export const listItem = [
       two: "주로 어떻게 해소하나요?",
     },
     items: [
-      { text: "운동으로 스트레스 해소", srcImg: <School />, score: 3 },
+      { text: "운동으로 스트레스 해소", srcImg: <School />, score: 1 },
       {
         text: "가족이나 친구와의 대화",
         srcImg: <Burning />,
-        score: 3,
+        score: 0,
       },
       {
         text: "음악 감상 또는 명상",
         srcImg: <Sleep />,
-        score: 3,
+        score: 0,
       },
       {
         text: "계획을 세우고 문제를 해결",
         srcImg: <Drawing />,
-        score: 3,
+        score: 1,
       },
       {
         text: "취미 활동 즐기기",
         srcImg: <Muscle />,
-        score: 3,
+        score: 0,
       },
       {
         text: "새로운 프로젝트나 도전에 집중",
         srcImg: <Family />,
-        score: 3,
+        score: 1,
       },
     ],
   },
@@ -149,32 +155,73 @@ export const listItem = [
       two: "어떤 모습인가요?",
     },
     items: [
-      { text: "목표지향적 개짱이", srcImg: <School />, score: 3 },
+      { text: "목표지향적 개짱이", srcImg: <School />, score: 1 },
       {
         text: "똑똑한 개짱이",
         srcImg: <Burning />,
-        score: 3,
+        score: 1,
       },
       {
         text: "여유로운 개짱이",
         srcImg: <Sleep />,
-        score: 3,
+        score: 0,
       },
       {
         text: "계획적인 개짱이",
         srcImg: <Drawing />,
-        score: 3,
+        score: 1,
       },
       {
         text: "행복한 개짱이",
         srcImg: <Muscle />,
-        score: 3,
+        score: 0,
       },
       {
         text: "즐거운 개짱이",
         srcImg: <Family />,
-        score: 3,
+        score: 0,
       },
     ],
+  },
+];
+
+export const surveyList: SurveyList[] = [
+  {
+    result1: {
+      title: "여유로운 베짱이",
+      srcImg: baejjange1,
+      subImg: minibaejjange,
+      subTitle1: "충분한 휴식을 통해 몸과 마음을 돌보는 당신,",
+      subTitle2: " 작은 도전으로 일상에 활력을 더해볼까요?",
+      score1: 2,
+      score2: 8,
+    },
+    result2: {
+      title: "균형잡힌 베짱이",
+      srcImg: baejjange2,
+      subImg: minibaejjange,
+      subTitle1: "충분한 휴식을 통해 몸과 마음을 돌보는 당신,",
+      subTitle2: " 작은 도전으로 일상에 활력을 더해볼까요?",
+      score1: 2,
+      score2: 8,
+    },
+    result3: {
+      title: "균형잡힌 개미",
+      srcImg: gaemi,
+      subImg: minigaemi,
+      subTitle1: "충분한 휴식을 통해 몸과 마음을 돌보는 당신,",
+      subTitle2: " 작은 도전으로 일상에 활력을 더해볼까요?",
+      score1: 2,
+      score2: 8,
+    },
+    result4: {
+      title: "부지런한 개미",
+      srcImg: gaemi2,
+      subImg: minigaemi,
+      subTitle1: "목표를 향해 끊임없이 노력하는 당신, ",
+      subTitle2: "건강한 성장을 위해 잠시 재충전 해볼까요?",
+      score1: 8,
+      score2: 2,
+    },
   },
 ];
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,24 @@
+import { StaticImageData } from "next/image";
+
 // export type SubjectType = "baejjange" | "gaemi";
 export enum SubjectType {
   baejjange = "baejjange",
   gaemi = "gaemi",
 }
+
+export type SurveyResult = {
+  title: string;
+  srcImg: StaticImageData;
+  subImg: StaticImageData;
+  subTitle1: string;
+  subTitle2: string;
+  score1: number;
+  score2: number;
+};
+
+export type SurveyList = {
+  result1: SurveyResult;
+  result2: SurveyResult;
+  result3: SurveyResult;
+  result4: SurveyResult;
+};
