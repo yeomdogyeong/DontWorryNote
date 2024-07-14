0d3d1647913abbbfa22004371cbc391e0fa8a291
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-14 18:27:38 +0900

    Feat:splash 페이지 구현

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { AuthButton } from "@/components/button/AuthButton";
+
+import Image from "next/image";
+import google from "../../../public/google.png";
+import kakao from "../../../public/kakao.png";
 
 function onBoarding() {
-  return <div>dd</div>;
+  return (
+    <main className="w-full h-full flex flex-col justify-between items-center">
+      <AuthButton text="KaKao로 시작하기" bgColor="kakao" srcImg={kakao} />
+      <AuthButton text="Google로 시작하기" bgColor="" srcImg={google} />
+    </main>
+  );
 }
 
 export default onBoarding;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
-import { AuthButton } from "@/components/button/AuthButton";
-
+"use client";
 import Image from "next/image";
-import google from "../../public/google.png";
-import kakao from "../../public/kakao.png";
-
+import slogan from "../../public/slogan.png";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import "../app/styles/animation.css";
 export default function Home() {
+  const router = useRouter();
+  const handleNextPage = () => {
+    setTimeout(() => router.push("/onboarding"), 3000);
+  };
+
+  useEffect(() => {
+    handleNextPage();
+  }, []);
   return (
-    <main className="w-full h-full flex flex-col justify-between items-center">
-      <AuthButton text="KaKao로 시작하기" bgColor="kakao" srcImg={kakao} />
-      <AuthButton text="Google로 시작하기" bgColor="" srcImg={google} />
+    <main className="w-full h-full flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center mb-[218px]">
+        <Image
+          src={slogan}
+          alt="sloganImg"
+          width={236}
+          height={33}
+          className="mb-[16px]"
+        />
+        <div className="text-gray-600">개짱이: 나를 찾아가는 루틴 여정</div>
+      </div>
     </main>
   );
 }
+//처음 Fadein
+//2초 뒤 onboarding page로 이동
