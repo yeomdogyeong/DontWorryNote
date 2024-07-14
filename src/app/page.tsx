9b2264fc7e058734c007ff726a155661ddd1e406
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-14 17:35:07 +0900

    Test: 임시 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import google from "../../public/google.png";
 import kakao from "../../public/kakao.png";
 
+import { SubjectType } from "@/types/common";
+import { BasicButton } from "@/components/button/BasicButton";
+import { PostButton } from "@/components/button/PostButton";
+
 export default function Home() {
   return (
     <main className="w-full h-full flex flex-col justify-between items-center">
