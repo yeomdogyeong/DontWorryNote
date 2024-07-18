5c3c8108a7a8d10f7794689c4ea480440d17c097
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-18 21:43:03 +0900

    Feat: axios interceptor request 추가

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -10,6 +10,21 @@ export const userInstance = (baseURL) => {
   return instance;
 };
 
+export const userInterceptorInstance = axios.interceptors.request.use(
+  function some(config) {
+    //토큰을 가져와서
+    const token = localStorage.setItem("tokenKey");
+    //요청의 header에 token 넣기
+    if (token) {
+      config.header["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 export const onBoarding = userInstance(
   "https://gaezzange.duckdns.org/api/v1/auth/token"
 );
