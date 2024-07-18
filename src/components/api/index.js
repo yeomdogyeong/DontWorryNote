9b0d6289a35449e128d05912c3dae9ad3989fd97
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-18 22:32:07 +0900

    Feat:axio response 추가

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -3,27 +3,38 @@ export const userInstance = (baseURL) => {
   const instance = axios.create({
     baseURL: baseURL,
     headers: {
-      "Content-type": "application/json",
+      "Content-Type": "application/json",
     },
     withCredentials: true,
   });
+
+  instance.interceptors.request.use(
+    function (config) {
+      // 토큰을 가져와서
+      const token = localStorage.getItem("tokenKey");
+      // 요청의 header에 token 넣기
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(function (response) {
+    return response;
+  }),
+    function (error) {
+      if (error.response && error.response.status === 401) {
+        alert("토큰이 만료되었습니다.");
+      }
+      return Promise.reject(error);
+    };
+
   return instance;
 };
-
-export const userInterceptorInstance = axios.interceptors.request.use(
-  function some(config) {
-    //토큰을 가져와서
-    const token = localStorage.setItem("tokenKey");
-    //요청의 header에 token 넣기
-    if (token) {
-      config.header["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
 
 export const onBoarding = userInstance(
   "https://gaezzange.duckdns.org/api/v1/auth/token"
