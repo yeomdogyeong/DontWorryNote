7ecb392a479f8c7420f61c985c23ce3dad477084
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-07-29 23:34:26 +0900

    Design: main 말풍선 삼각형 만들기 초안

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -51,14 +51,19 @@ export default function HomePage() {
       </div>
 
       <div
-        className={` flex flex-col text-white self-start justify-center items-center mb-[20px] w-[200px] sm:w-[270px] sm:h-[100px] sm:text-[19px] ml-[20px] border-none p-4  text-[14px] rounded-xl ${
-          tendency === "GAEMI" ? "bg-[#505866]" : "bg-[#649C7D]"
-        }`}
+        className={` flex flex-col text-white self-start justify-center items-center mb-[20px] w-[200px] sm:w-[270px] sm:h-[100px] sm:text-[19px]
+           ml-[20px] border-none p-4  text-[14px] rounded-xl ${
+             tendency === "GAEMI" ? "bg-[#505866]" : "bg-[#649C7D]"
+           }`}
       >
         <div>오늘도 행복한 하루를 보내세요. </div>
         <div>제가 옆에서 열심히 응원할게요!</div>
       </div>
-
+      {/* <div
+        className={`w-5 h-5 border-l-[4px] border-r-[4px] border-b-[7px] border-l-transparent border-r-transparent ${
+          tendency === "GAEMI" ? "border-b-[#505866]" : "border-b-[#649C7D]"
+        }`}
+      ></div> */}
       <OptionDialog
         data={data?.data.data}
         todayDate={todayDate}
