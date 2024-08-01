bae9b9541946489deb8ae368a41ef9fd7dd8644b
박진우 <pjw9195@naver.com>
2024-08-01 14:32:53 +0900

    fix: 통계값 수정

--- src/components/report/WeekDayReport.tsx
+++ src/components/report/WeekDayReport.tsx
@@ -56,27 +56,27 @@ export default function WeekDayReport() {
 
   const gaemiPercent = useMemo(() => {
     const value =
-      ((successCount[0] + successCount[1]) / successCount[0]) * 100 === Infinity
+      (successCount[0] / (successCount[0] + successCount[1])) * 100 === Infinity
         ? 0
-        : ((successCount[0] + successCount[1]) / successCount[0]) * 100;
+        : (successCount[0] / (successCount[0] + successCount[1])) * 100;
 
     if (Number.isNaN(value)) {
       return 0;
     } else {
-      return value;
+      return Math.round(value);
     }
   }, [successCount]);
 
   const baezzangePercent = useMemo(() => {
     const value =
-      ((successCount[0] + successCount[1]) / successCount[1]) * 100 === Infinity
+      (successCount[1] / (successCount[0] + successCount[1])) * 100 === Infinity
         ? 0
-        : ((successCount[0] + successCount[1]) / successCount[1]) * 100;
+        : (successCount[1] / (successCount[0] + successCount[1])) * 100;
 
     if (Number.isNaN(value)) {
       return 0;
     } else {
-      return value;
+      return Math.round(value);
     }
   }, [successCount]);
 
@@ -117,7 +117,7 @@ export default function WeekDayReport() {
       );
     }
 
-    if (gaemiPercent > 50) {
+    if (gaemiPercent >= 50) {
       return (
         <div className="circle relative overflow-hidden border-[#464343] rounded-full bg-gray-100 translate3d(0, 0, 0) w-[100px] h-[100px]">
           <div
